d3553b5cbfdb6337c26707a5d2d1210e25d1dd19
Keren Aywila <kerensss64@gmail.com>
2024-07-24 09:19:19 +0100

    first commit

---
+++ script..js
@@ -0,0 +1,39 @@
+//String
+
+const firstName = "Keren Happuch";
+const middleName = "Maidariya";
+const lastName = "Aywila";
+const stateOrigin = "Plateau";
+const profession = "Engineer";
+
+//Numbers
+
+const chemistry = 60;
+const biology = 78;
+const physics = 80;
+const mathematics = 79;
+const englishLanguage = 92;
+
+//Booleans
+
+const isRed = false;
+const isCorrect = true;
+const hasPlayed = true;
+const isNew = true;
+const isComplete = false;
+
+//Null
+
+const empty = null
+const fileA = null
+const data = null
+const result = null
+const total = null
+
+//Undefined
+
+let playerA;
+let playerB;
+let playerC;
+let playerD;
+let playerE;
